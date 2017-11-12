d5ef09548b476c4e37d205d41df0352219ab5fa1
kszczurowski <kszczurowski@absolute.com>
2017-11-11 23:54:08 -0800

    Add project files.

---
+++ BCIT_WPF_COMP3608_Week3Lab4/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BCIT_WPF_COMP3608_Week3Lab4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
