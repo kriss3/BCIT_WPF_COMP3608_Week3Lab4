855efbdcc7f0c8d04945b463f465b91d775b2f63
kszczurowski <kszczurowski@absolute.com>
2017-11-12 00:32:02 -0800

    Inital Commit. Working Lab4 for Event Handling.

---
+++ BCIT_WPF_COMP3608_Week3Lab4/RoutedEvents.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BCIT_WPF_COMP3608_Week3Lab4
+{
+    /// <summary>
+    /// Interaction logic for RoutedEvents.xaml
+    /// Krzysztof Szczurowski
+    /// Repo address: https://github.com/kriss3/BCIT_WPF_COMP3608_Week3Lab4.git
+    /// </summary>
+    public partial class RoutedEvents : Window
+    {
+        public RoutedEvents()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show("Event raised by Textbox");
+            e.Handled = (bool)rb_ForTextBox.IsChecked;
+        }
+
+        private void Grid_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show("Event raised by Grid");
+            e.Handled = (bool)rb_ForGrid.IsChecked;
+        }
+
+        private void Window_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show("Event raised by Window");
+            e.Handled = (bool)rb_ForWindow.IsChecked;
+        }
+    }
+}
