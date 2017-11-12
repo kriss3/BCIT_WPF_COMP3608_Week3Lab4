945530af3134accd71784d684b873829e79943da
kszczurowski <kszczurowski@absolute.com>
2017-11-12 15:27:47 -0800

    Changing main window mode to NoResize; adding better messageBox handling, comments;

--- BCIT_WPF_COMP3608_Week3Lab4/RoutedEvents.xaml.cs
+++ BCIT_WPF_COMP3608_Week3Lab4/RoutedEvents.xaml.cs
@@ -17,19 +17,19 @@ namespace BCIT_WPF_COMP3608_Week3Lab4
 
         private void TxtBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            MessageBox.Show("Event raised by Textbox");
+            MessageBox.Show("Event raised by Textbox", "Event Message", MessageBoxButton.OK, MessageBoxImage.Information);
             e.Handled = (bool)rb_ForTextBox.IsChecked;
         }
 
         private void Grid_TextChanged(object sender, TextChangedEventArgs e)
         {
-            MessageBox.Show("Event raised by Grid");
+            MessageBox.Show("Event raised by Grid", "Event Message", MessageBoxButton.OK, MessageBoxImage.Information);
             e.Handled = (bool)rb_ForGrid.IsChecked;
         }
 
         private void Window_TextChanged(object sender, TextChangedEventArgs e)
         {
-            MessageBox.Show("Event raised by Window");
+            MessageBox.Show("Event raised by Window", "Event Message", MessageBoxButton.OK, MessageBoxImage.Information);
             e.Handled = (bool)rb_ForWindow.IsChecked;
         }
     }
